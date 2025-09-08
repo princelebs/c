fdb5ccfeb7244bfab8f4e67459289093c701914a
prince chovatiya <princepatle0720@gmail.com>
2025-09-08 10:19:40 +0530

    Add files via upload

---
+++ assignment-2/A2P4M2.C
@@ -0,0 +1,18 @@
+// Conditional Operator Program 4
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int num;
+    clrscr();
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    (num < 0) ? printf("%d is a negative number.", num) :
+    (num > 0) ? printf("%d is a positive number.", num) :
+                printf("%d is zero.", num);
+
+    getch();
+}
---
+++ assignment-2/A2P6.C
@@ -0,0 +1,68 @@
+/*
+This program inputs marks of 4 subjects for 3 students, calculates total and percentage,
+and assigns grade based on percentage using if statements
+Date: 7th Aug 2025
+author: prince chovatiya
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int c, html, cf, math, total, percentage;
+	int i = 1;  
+	clrscr();
+
+	while(i <= 3)
+	{
+		printf("\n Student %d", i);
+		printf("Input Marks of C: ");
+		scanf("%d", &c);
+		printf("Input Marks of HTML: ");
+		scanf("%d", &html);
+		printf("Input Marks of CF: ");
+		scanf("%d", &cf);
+		printf("Input Marks of Math: ");
+		scanf("%d", &math);
+
+		total = c + html + cf + math;
+		percentage = total / 4;
+
+		printf("Total Marks: %d\n", total);
+		printf("Percentage: %d%%\n", percentage);
+
+
+		if(percentage >= 90)
+		{
+			printf("Grade: A++\n");
+		}
+		else if(percentage >= 80)
+		{
+			printf("Grade: A+\n");
+		}
+		else if(percentage >= 70)
+		{
+			printf("Grade: A\n");
+		}
+		else if(percentage >= 60)
+		{
+			printf("Grade: B\n");
+		}
+		else if(percentage >= 50)
+		{
+			printf("Grade: C\n");
+		}
+		else if(percentage >= 40)
+		{
+			printf("Grade: D\n");
+		}
+		else
+		{
+			printf("Grade: FAIL\n");
+		}
+ 
+	}
+
+	getch();
+}
