62e40a956ce3eff33f2264d071156b54272d6d43
prince chovatiya <princepatle0720@gmail.com>
2025-09-13 10:03:08 +0530

    Add files via upload

---
+++ assignment-3/PG17.C
@@ -0,0 +1,25 @@
+/*
+This program can print sum of n digits that was inputed by user
+Date: 13th sept 2025
+Author: prince chovatiya
+*/
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num=0,digit=0,sum=0;
+	clrscr();
+	printf("enter number : ");
+	scanf("%d",&num);
+
+	while(num !=0)
+	{
+		digit=num%10;
+		sum=sum+digit;
+		num=num/10;
+	}
+
+	printf("sum of digit :%d",sum);
+	getch();
+}
